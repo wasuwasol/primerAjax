6a219d608a5466c177530fbcbde536fbed620828
Jorge Schenone <jschenone88@gmail.com>
2019-09-30 22:04:13 -0300

    ampliacion de promesas

--- js/main.js
+++ js/main.js
@@ -25,16 +25,32 @@ const mostrarData = function (_respuestaDeAjax) { //API fetch
 
 
     valores.forEach((_element, _key) => {
+
         let li = document.createElement('li');
         if( _element.includes('https')){
             fetch(_element)
-            .then(_respuesta => _respuesta.json())
+            .then(_respuesta => _respuesta.json()) // _respuesta.json() puede ser una callback
             .then(_obj => {
                 if (keys[_key] === 'homeworld') li.append(keys[_key], ': ', _obj.name);
                 else if (keys[_key] === 'url') li.append(keys[_key], ': ', _obj.url);
             })
         }else if(Array.isArray(_element)){
-            
+            let ul2 = document.createElement('ul');
+            let h3 = document.createElement('h3');
+            h3.innerText = keys[_key];
+            ul2.append(h3);
+            _element.forEach( _e => {
+                fetch(_e)
+                .then( _res => _res.json())
+                .then( _obj => {
+                    for (const key in _obj) {
+                        let li2 = document.createElement('li');
+                        li2.append(key,': ', _obj[key]);
+                        ul2.append(li2);
+                    }
+                })
+                li.append(ul2);
+            })
             console.log('hay arrays en '+ keys[_key] );
         }else{
             li.append(keys[_key], ': ', valores[_key]);        
